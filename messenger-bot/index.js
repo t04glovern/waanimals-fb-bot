993cb31b4831792a47d3c2f791e8a781b98f534b
Nathan Glover <nathan@glovers.id.au>
2019-05-05 13:47:53 +0800

    Basic image read PoC complete

--- messenger-bot/index.js
+++ messenger-bot/index.js
@@ -1,21 +1,25 @@
 'use strict';
 
+require('dotenv').config();
+
 // Imports dependencies and set up http server
 const
-  dotenv = require('dotenv'),
+  request = require('request'),
   express = require('express'),
   bodyParser = require('body-parser'),
   app = express().use(bodyParser.json()); // creates express http server
 
-// Use .env variables
-dotenv.config();
-
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
-// Creates the endpoint for our webhook 
+/**
+ * 
+ * Webhook POST
+ * 
+ */
 app.post('/webhook', (req, res) => {
 
   let body = req.body;
@@ -30,6 +34,18 @@ app.post('/webhook', (req, res) => {
       // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log("Sender PSID: " + sender_psid);
+
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      }
     });
 
     // Returns a '200 OK' response to all requests
@@ -41,7 +57,11 @@ app.post('/webhook', (req, res) => {
 
 });
 
-// Adds support for GET requests to our webhook
+/**
+ * 
+ * Webhook GET
+ * 
+ */
 app.get('/webhook', (req, res) => {
   // Parse the query params
   let mode = req.query['hub.mode'];
@@ -65,6 +85,115 @@ app.get('/webhook', (req, res) => {
   }
 });
 
+/**
+ * 
+ * Health Endpoint
+ * 
+ */
 app.get("/healthy", (req, res) => {
   res.status(200).send("Health endpoint successful!");
 });
+
+/**
+ * 
+ * Handler Postback
+ * 
+ */
+function handlePostback(sender_psid, received_postback) {
+  console.log('ok')
+  let response;
+  // Get the payload for the postback
+  let payload = received_postback.payload;
+
+  // Set the response based on the postback payload
+  if (payload === 'yes') {
+    response = {
+      "text": "Thanks!"
+    }
+  } else if (payload === 'no') {
+    response = {
+      "text": "Oops, try sending another image."
+    }
+  }
+  // Send the message to acknowledge the postback
+  callSendAPI(sender_psid, response);
+}
+
+/**
+ * 
+ * Handler Message
+ * 
+ */
+function handleMessage(sender_psid, received_message) {
+  let response;
+
+  // Checks if the message contains text
+  if (received_message.text) {
+    // Create the payload for a basic text message, which
+    // will be added to the body of our request to the Send API
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+    }
+  } else if (received_message.attachments) {
+    // Get the URL of the message attachment
+    let attachment_url = received_message.attachments[0].payload.url;
+    response = {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "generic",
+          "elements": [{
+            "title": "Is this the right picture?",
+            "subtitle": "Tap a button to answer.",
+            "image_url": attachment_url,
+            "buttons": [{
+                "type": "postback",
+                "title": "Yes!",
+                "payload": "yes",
+              },
+              {
+                "type": "postback",
+                "title": "No!",
+                "payload": "no",
+              }
+            ],
+          }]
+        }
+      }
+    }
+  }
+
+  // Send the response message
+  callSendAPI(sender_psid, response);
+}
+
+/**
+ * 
+ * Messenger Send API
+ * 
+ */
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": {
+      "access_token": PAGE_ACCESS_TOKEN
+    },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  });
+}
